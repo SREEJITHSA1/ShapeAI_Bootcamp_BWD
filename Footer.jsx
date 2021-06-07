4898577bb486db0ffe477dcaf3adac9c895e0d87
SREEJITH S A <84565847+SREEJITHSA1@users.noreply.github.com>
2021-06-07 19:41:15 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 function Footer() {
-
+  var currYear = new Date().getFullyear();
+  
   return (
     <footer>
-      <p>Copyright @ 2021</p>
+      <p>Copyright @ {currYear}</p>
     </footer>
   );
 }
