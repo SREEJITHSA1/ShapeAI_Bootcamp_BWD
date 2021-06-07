ba24ab44e10df6cd55cc0495df3a9d74be6ca722
SREEJITH S A <84565847+SREEJITHSA1@users.noreply.github.com>
2021-06-07 10:55:24 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amaizing 7 days bootcamp taken up by Shaurya Sinha sir. They
+        covered everything from Scrach including Javascript, React.Js, HTML. I
+        like this session very much.I am new to javascript and web
+        development.This bootcamp was very useful to me. Thank you Shaury Sinha
+        Sir and Shape AI for this awesome session .The examples that Shaurya sir
+        put forward was so simple that everyone can understand easily. As a
+        beginner this bootcamp helped me to gain a lot of knowledge .Once again
+        Thank you very much ...
+      </p>
+    </div>
+  );
+}
+export default Note;
